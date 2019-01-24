2ea7937c5175f23e48969d6394a8a8568394082f
Kimberly Rouse <ksrouse94@gmail.com>
2019-01-24 23:57:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,12 +49,12 @@ class TicTacToe
   def turn 
     input = gets
     input_to_index(input)
-    #if valid_move?(input) == TRUE
-      #move(input, current_player(board))
-      #display_board
-    #elsif valid_move?(board, input) != TRUE
-      #turn
-    #end 
+    if valid_move?(input) == TRUE
+      move(input, current_player(board))
+      display_board
+    elsif valid_move?(board, input) != TRUE
+      turn
+    end 
   end 
   
   def turn_count
